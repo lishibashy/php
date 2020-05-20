a180ce5a8c21d18deec4e6c8db42718ce897f8bc
lishibashy <luciano.ishibashy@gmail.com>
2020-05-20 11:25:43 -0300

    Formulario de Cadastro de Vendas

---
+++ dashboard/cadastro_vendas.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Vendas</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <div class="container">
+        <h2> Cadastro de Vendas</h2>
+        <form action="">
+                <div class="form-group">
+                    <label>Selecione o mês</label>
+                    <select class="form-control">
+                        <option value="">Jan</option>
+                        <option value="">Fev</option>
+                        <option value="">Mar</option>
+                        <option value="">Abr</option>
+                        <option value="">Mai<a</option>
+                        <option value="">Jun</option>
+                        <option value="">Jul</option>
+                        <option value="">Ago</option>
+                        <option value="">Set</option>
+                        <option value="">Out</option>
+                        <option value="">Nov</option>
+                        <option value="">Dez</option>
+                    </select>
+                </div>
+                    <div class="form-group">
+                        <label>Digite a quantidade</label>
+                        <input type="number" name="quantidade" id="" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Digite o valor</label>
+                        <input type="number" name="valor" id="" class="form-control">
+                    </div>
+                    <div style="text-align: right">
+                        <button type="submit" class="btn btn-primary">Cadastrar</button>
+                    </div>
+            
+        </form>
+    </div>
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</body>
+</html>

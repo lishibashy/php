4306515649282ece93c541f9a2dfa55e4c382af2
lishibashy <luciano.ishibashy@gmail.com>
2020-05-23 14:47:20 -0300

    Testando Div background colors

---
+++ dashboard/teste_div_colors.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+</head>
+<body>
+    
+    <div class="container" style="background-color: firebrick">
+        <div class="row" style="background-color: darkorchid">
+            <div class="col-md-6" style="background-color: darksalmon">
+                Container 01
+            </div>
+            <div class="col-md-6">
+                TESTE 02
+            </div>
+            <div class="alert alert-primary" role="alert">
+             A simple primary alert—check it out!
+        </div>
+        </div>
+    </div>
+    <div class="container-fluid" style="background-color: firebrick">
+        <div class="row" style="background-color: darkorchid">
+            <div class="col-md-6" style="background-color: darksalmon">
+                Container-fluid 01
+            </div>
+            <div class="col-md-6">
+                TESTE 02
+            </div>
+        </div>
+        <div class="alert alert-primary" role="alert">
+             A simple primary alert—check it out!
+        </div>
+    </div>
+ 
+
+
+</body>
+</html>
